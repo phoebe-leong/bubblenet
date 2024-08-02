6c2ecbcdbb2be3e9c6d12c2fcc5ec14d755ff7b8
pwompus <dariusleong8@gmail.com>
2024-08-02 16:17:58 +1000

    added support for dynamic post page

--- frontend/postRequest.js
+++ frontend/postRequest.js
@@ -1,7 +1,13 @@
 window.onload = async () => {
 	const id = window.location.pathname.split('/')[2]
-	console.log(id)
-
 	const data = await fetch(`http://127.0.0.1:8000/data/${id}.json`)
 		.then((data) => data.json())
+
+	if (data.hasMedia) {
+		document.querySelector("img").src = data.mediaLink
+	}
+
+	document.getElementById("post-text").innerHTML = data.content
+	document.getElementById("unix-time").innerHTML = `Posted: ${data.unixTimestamp} (UTC+10)`
+	document.getElementById("post-id").innerHTML = `Post Id: ${id}`
 }
