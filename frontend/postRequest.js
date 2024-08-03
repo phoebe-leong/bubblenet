d95d8c18b9139e932354def342167df17e073d2d
pwompus <dariusleong8@gmail.com>
2024-08-03 23:16:49 +1000

    add image modal to show post image bigger

--- frontend/postRequest.js
+++ frontend/postRequest.js
@@ -9,7 +9,23 @@ window.onload = async () => {
 		document.querySelector("img").src = "/media/image-placeholder.png"
 	}
 
+	const container = document.createElement("dialog")
+	const image = document.createElement("img")
+		image.id = "modal"
+		image.src = (data.hasMedia) ? data.mediaLink : "/media/image-placeholder.png"
+	const closeBtn = document.createElement("img")
+		closeBtn.id = "close"
+		closeBtn.src = "/media/close-icon.png"
+		closeBtn.onclick = () => {
+			document.querySelector("dialog").close()
+		}
+
+	container.appendChild(image)
+	container.appendChild(closeBtn)
+
 	document.getElementById("post-text").innerHTML = data.content
 	document.getElementById("unix-time").innerHTML = `Posted: ${data.unixTimestamp} (UTC+10)`
 	document.getElementById("post-id").innerHTML = `Post Id: ${id}`
+
+	document.body.appendChild(container)
 }
