aae6bd4b7c98a58483cbb376fd78211dd427f043
pwompus <dariusleong8@gmail.com>
2024-08-06 21:23:19 +1000

    fix section loading and fix version bug

--- frontend/archive.js
+++ frontend/archive.js
@@ -41,42 +41,41 @@ function placeArchiveItem(item) {
 	const sections = Array.prototype.slice.call(document.getElementById("archive").children)
 
 	for (let i = 0; i < sections.length; i++) {
-		const columns = Array.prototype.slice.call(sections[i].children)
-
+		const columns = sections[i].children
 		for (let j = 0; j < columns.length; j++) {
 			if (Array.prototype.slice.call(columns[j].children).length < 6) {
 				columns[j].appendChild(item)
 				return
 			}
-		}
 
-		if (columns.length < 5) {
-			const column = document.createElement("div")
-				column.classList.add("column")
+			if (columns.length < 5) {
+				const column = document.createElement("div")
+					column.classList.add("column")
 
-			column.appendChild(item)
-			sections[i].appendChild(column)
+				column.appendChild(item)
+				sections[i].appendChild(column)
+
+				return
+			}
 		}
 	}
 
-	if (sections.length == 0 || (sections[sections.length - 1].children.length == 6 && Array.prototype.slice.call(sections[sections.length - 1].lastElementChild.children).length == 5)) {
-		const section = document.createElement("section")
-		const column = document.createElement("div")
-			column.classList.add("column")
+	const section = document.createElement("section")
+	const column = document.createElement("div")
+		column.classList.add("column")
 
-		column.appendChild(item)
-		section.appendChild(column)
+	column.appendChild(item)
+	section.appendChild(column)
 
-		document.getElementById("archive").appendChild(section)
-	}
+	document.getElementById("archive").appendChild(section)
 }
 
 async function fetchArchive() {
-	if (document.querySelector(".column") != null) {
-		const columns = Array.prototype.slice.call(document.getElementById("archive").children)
+	if (document.querySelector("section") != null) {
+		const sections = Array.prototype.slice.call(document.getElementById("archive").children)
 
-		for (let i = 0; i < columns.length; i++) {
-			columns[i].remove()
+		for (let i = 0; i < sections.length; i++) {
+			sections[i].remove()
 		}
 	}
 
