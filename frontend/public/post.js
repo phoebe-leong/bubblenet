c187c6acdb3494aa865358819a049651e5137f2e
pwompus <dariusleong8@gmail.com>
2024-08-11 15:43:14 +1000

    removed different id for opera browser

--- frontend/public/post.js
+++ frontend/public/post.js
@@ -68,11 +68,5 @@ window.onload = async () => {
 
 	document.getElementById("version").style.paddingBottom = "12px"
 
-	const isSpyware = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0
-
-	if (isSpyware) {
-		document.getElementById("extra-info").id = "extra-info-opera"
-	}
-
 	setExtraInfoDynamicMargin()
 }
