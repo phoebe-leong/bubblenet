53e854c179f2b9ad553f4625f090bc361e95c7bb
pwompus <dariusleong8@gmail.com>
2024-10-04 16:44:54 +1000

    delete everything

--- frontend/public/redirect.js
+++
@@ -1,5 +0,0 @@
-const PORT = 8000
-
-if (window.location.host == `127.0.0.1:${PORT}` || window.location.host == `[::1]:${PORT}`) {
-	window.location.href = `http://localhost:${PORT}${window.location.pathname}`
-}
--- frontend/public/subheader.js
+++
@@ -1,7 +0,0 @@
-async function subheader() {
-	const subheader = await fetch("/data/subheader.txt")
-		.then((data) => data.text())
-
-	document.getElementById("subheader").innerHTML = subheader
-}
-subheader()
