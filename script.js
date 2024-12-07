56ed44efc0b3f8ad58a94d7110a71e02bdf9737f
pwompus <dariusleong8@gmail.com>
2024-12-08 01:13:51 +1100

    added line breaks and added dialog boxes to images

---
+++ script.js
@@ -0,0 +1,26 @@
+let dialogs = []
+
+function createDialogs() {
+	const imgSrcs = ["feed.png", "archive.png", "new-post.png", "light-mode.png", "post.png"]
+
+	for (const i in imgSrcs) {
+		const dialog = document.createElement("dialog")
+		const img = document.createElement("img")
+			img.src = `media/${imgSrcs[i]}`
+
+		dialog.addEventListener("close", () => {
+			document.body.style.overflow = "auto"
+		})
+
+		dialog.appendChild(img)
+		document.body.appendChild(dialog)
+		dialogs.push(dialog)
+	}
+}
+
+function showDialog(index) {
+	dialogs[index].showModal()
+	document.body.style.overflow = "hidden"
+}
+
+createDialogs()
